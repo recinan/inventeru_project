bbb5adb18a80482efefa0481085d380fe3a6e06a
Recep Inan <recinan.ceng@gmail.com>
2024-10-23 19:00:52 +0300

    Register, Login and Logout added

--- inventory_management_con/accounts/views.py
+++ inventory_management_con/accounts/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from accounts.forms import UserRegisterForm, UserLoginForm
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'register.html', {
+        'form' : form 
+        })
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    auth_login(request, user)
+                    return redirect('index')
+    else:
+        form = UserLoginForm()
+    
+    return render(request, 'login.html', {
+        'form':form
+    })
+
+def logout(request):
+    auth_logout(request)
+    return render(request, 'logout.html')
+
