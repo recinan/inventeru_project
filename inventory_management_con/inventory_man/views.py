aea4f7d1a980936df3f2f21aecf9731288c00305
Recep Inan <recinan.ceng@gmail.com>
2024-10-26 13:54:11 +0300

    Edit view has added

--- inventory_management_con/inventory_man/views.py
+++ inventory_management_con/inventory_man/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from django.urls import reverse_lazy
 from .models import InventoryItem,Category
 from django.contrib.auth.decorators import login_required
@@ -21,7 +21,7 @@ def add_item(request):
     if request.method == 'POST':
         form = InventoryItemForm(request.POST)
         if form.is_valid():
-            item = form.save()
+            item = form.save(commit=False)
             item.user = request.user
             item.save()
             print(request.user)
@@ -34,4 +34,21 @@ def add_item(request):
         'form':form,
         'categories':categories
     }
+    return render(request, 'item_form.html',context)
+
+@login_required
+def edit_item(request, pk):
+    item = get_object_or_404(InventoryItem, pk = pk)
+    if request.method == 'POST':
+        form = InventoryItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse_lazy('dashboard'))
+    else:
+        form = InventoryItemForm(instance=item)
+
+    context = {
+        'form':form
+    }
+
     return render(request, 'item_form.html',context)
