f040e00aea543e0718a68404d3cf35a39567b058
Recep Inan <recinan.ceng@gmail.com>
2024-10-26 13:53:48 +0300

    Urls has updated

--- inventory_management_con/inventory_man/urls.py
+++ inventory_management_con/inventory_man/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('dashboard/',views.dashboard, name='dashboard'),
-    path('add-item/',views.add_item, name='add-item')
+    path('add-item/',views.add_item, name='add-item'),
+    path('edit-item/<int:pk>', views.edit_item, name='edit-item')
 ]
