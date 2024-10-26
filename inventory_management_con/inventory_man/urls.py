a12a21824309e0913bdddb2cad05218ad2d6de0f
Recep Inan <recinan.ceng@gmail.com>
2024-10-26 14:32:56 +0300

    Url page has updated

--- inventory_management_con/inventory_man/urls.py
+++ inventory_management_con/inventory_man/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('dashboard/',views.dashboard, name='dashboard'),
     path('add-item/',views.add_item, name='add-item'),
-    path('edit-item/<int:pk>', views.edit_item, name='edit-item')
+    path('edit-item/<int:pk>', views.edit_item, name='edit-item'),
+    path('delete-item/<int:pk>', views.delete_item, name='delete-item')
 ]
