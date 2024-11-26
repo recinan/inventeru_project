742d51ef89fc35c22bc6d8eae6dabcd8cd9d652e
Recep Inan <recinan.ceng@gmail.com>
2024-11-26 21:17:18 +0300

    CategoryForm has added

--- inventory_management_con/inventory_man/forms.py
+++ inventory_management_con/inventory_man/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from inventory_man.models import Category, InventoryItem
+from warehouse_man.models import Warehouse
 
 class InventoryItemForm(forms.ModelForm):
     category = forms.ModelChoiceField(queryset=Category.objects.all(), initial=0)
     class Meta:
         model = InventoryItem
-        fields = ['name', 'quantity','category']
+        fields = ['name', 'quantity','category','warehouse']
+
+class CategoryForm(forms.ModelForm):
+    category = forms.CharField()
+    class Meta:
+        model = Category
+        fields = ['category']
