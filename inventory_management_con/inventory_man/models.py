16533de711959a34cbdfed188dda77037cb66b36
Recep Inan <recinan.ceng@gmail.com>
2024-11-30 22:43:01 +0300

    InventoryItem model has updated

--- inventory_management_con/inventory_man/models.py
+++ inventory_management_con/inventory_man/models.py
@@ -7,7 +7,7 @@ class InventoryItem(models.Model):
     name = models.CharField(max_length=200)
     quantity = models.IntegerField()
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    #warehouse = models.ForeignKey('warehouse_man.Warehouse', on_delete=models.SET_NULL, blank=True,null=True)
+    warehouse = models.ForeignKey('warehouse_man.Warehouse', on_delete=models.SET_NULL, blank=True,null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
