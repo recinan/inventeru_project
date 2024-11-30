c06ed94fd7c1da004f31d440be25e6f6c264f331
Recep Inan <recinan.ceng@gmail.com>
2024-11-30 22:52:51 +0300

    WarehouseForm form has updated

--- inventory_management_con/warehouse_man/forms.py
+++ inventory_management_con/warehouse_man/forms.py
@@ -6,4 +6,15 @@ class WarehouseForm(forms.ModelForm):
     class Meta:
         model = Warehouse
         fields = ['warehouse_name']
-    
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user',None)
+        super().__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        if self.user:
+            instance.user = self.user
+        if commit:
+            instance.save()
+        return instance
