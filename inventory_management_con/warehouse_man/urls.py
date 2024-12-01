57b24144c21c1cc591262a8fb930ed2fdce5f09b
Recep Inan <recinan.ceng@gmail.com>
2024-12-01 22:57:40 +0300

    warehouse_man urls updated

--- inventory_management_con/warehouse_man/urls.py
+++ inventory_management_con/warehouse_man/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('list-warehouse',views.list_warehouse, name='list-warehouse'),
     path('add-warehouse',views.add_warehouse,name='add-warehouse')
 ]
