097080aeffe829ee39aa3f3ba22a69705f68335a
Recep Inan <recinan.ceng@gmail.com>
2024-12-02 20:13:04 +0300

    list categories view added

--- inventory_management_con/category_man/views.py
+++ inventory_management_con/category_man/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.urls import reverse_lazy
 from .models import Category
 from .forms import CategoryForm
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from warehouse_man.models import Warehouse
 
 # Create your views here.
 
@@ -27,3 +28,11 @@ def get_categories_for_warehouse(request, warehouse_id):
     categories = Category.objects.filter(warehouse_id=warehouse_id)
     category_list = [{"id": category.id, "name": category.category_name} for category in categories]
     return JsonResponse({"categories": category_list})
+
+def list_categories(request, warehouse_slug):
+    warehouse = get_object_or_404(Warehouse, slug = warehouse_slug)
+    category_list = Category.objects.filter(user = request.user, warehouse = warehouse)
+    context = {
+        'categorylist':category_list
+    }
+    return render(request, 'list_category.html', context)
