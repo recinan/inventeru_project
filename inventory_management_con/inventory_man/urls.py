e84620f83625590c4d0f0c9a0ebc520629340a16
Recep Inan <recinan.ceng@gmail.com>
2024-12-02 22:18:29 +0300

    urls update

--- inventory_management_con/inventory_man/urls.py
+++ inventory_management_con/inventory_man/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('dashboard/',views.dashboard, name='dashboard'),
     path('add-item/',views.add_item, name='add-item'),
+    path('list-item/<slug:category_slug>/',views.list_item, name='list-item'),
     path('edit-item/<int:pk>', views.edit_item, name='edit-item'),
     path('delete-item/<int:pk>', views.delete_item, name='delete-item')
 ]
