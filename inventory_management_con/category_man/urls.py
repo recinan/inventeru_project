1619e069cae570cc56f44cce2ee70996f0eb3c49
Recep Inan <recinan.ceng@gmail.com>
2024-12-03 17:13:05 +0300

    url path update

--- inventory_management_con/category_man/urls.py
+++ inventory_management_con/category_man/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('add-category', views.add_category,name='add-category'),
-    path('list-category/<slug:warehouse_slug>/', views.list_categories, name='list-category'),
+    path('list-warehouse/list-category/<slug:warehouse_slug>/', views.list_categories, name='list-category'),
+    path('list-allcategories',views.list_allcategories,name='list-allcategories'),
     path('get-categories-for-warehouse/<int:warehouse_id>/', views.get_categories_for_warehouse, name='get_categories_for_warehouse')
 ]
