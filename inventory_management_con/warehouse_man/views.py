1ae821c19c7d36ef1d240ab0aad2c641dd792784
Recep Inan <recinan.ceng@gmail.com>
2024-12-12 20:41:26 +0300

    warehouse product list pdf added

--- inventory_management_con/warehouse_man/views.py
+++ inventory_management_con/warehouse_man/views.py
@@ -5,6 +5,12 @@ from .forms import WarehouseForm
 from .models import Warehouse
 from inventory_man.models import InventoryItem
 from category_man.models import Category
+from django.http import FileResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
 
 # Create your views here.
 
@@ -62,3 +68,66 @@ def warehouse_detail(request, warehouse_slug):
     }
     
     return render(request, 'warehouse_man/warehouse-detail.html',context)
+
+def warehouse_detail_pdf(request, warehouse_slug):
+    warehouse = get_object_or_404(Warehouse, user = request.user,slug = warehouse_slug)
+    categories = Category.objects.filter(user = request.user, warehouse=warehouse)
+    items = InventoryItem.objects.filter(user = request.user, warehouse=warehouse)
+
+    buf = io.BytesIO()
+    c = canvas.Canvas(buf, pagesize=letter,bottomup=0)
+
+    page_width, page_height = letter
+
+    c.setFont("Helvetica-Bold",14)
+    c.drawString(80,30,f"{warehouse.warehouse_name}")
+    c.setLineWidth(3)
+    c.line(50,50,page_width-50,50)
+
+    #warehouse info text object
+    warehouse_info_textob = c.beginText()
+    warehouse_info_textob.setTextOrigin(180, 20)
+    warehouse_info_textob.setFont("Helvetica",8)
+    warehouse_info_textob.textLine(f"Adress: {warehouse.neighborhood} {warehouse.street} {warehouse.district} / {warehouse.city} {warehouse.country} {warehouse.postal_code}")
+    warehouse_info_textob.textLine("-----------------------------------------------------------------------------------------------------------------------------------------")
+    warehouse_info_textob.textLine(f"Phone Number: {warehouse.phone_number}")
+    #warehouse_info_textob.textLine(f"Total Number of Categories: {categories.count()} - Total Number of Items: {items.count()}")
+    c.setLineWidth(1)
+    #c.line(50,100,page_width-50,100)
+    c.drawText(warehouse_info_textob)
+    
+
+    #Products info text object
+    products = c.beginText()
+    products.setTextOrigin(80,inch)
+    products.setFont("Courier",10)
+    table_header = ("C".ljust(5) 
+                    + "Name".ljust(15) 
+                    + "Qty".ljust(5) 
+                    + "Price".ljust(15) 
+                    + "Description".ljust(20) 
+                    + "Category".ljust(20))
+    products.textLine(table_header)
+    products.textLine("-"*80)
+    lines = []
+    i = 1
+    for item in items:
+        line = (f"{str(i)}-".ljust(5)
+        + f"{item.item_name}".ljust(15) 
+        + f"{item.quantity}".ljust(5)
+        + f"{item.price} {item.currency}".ljust(15)
+        + f"{item.description[:20]}".ljust(20) 
+        + f"{item.category}".ljust(20) )
+        #lines.append(f"{i}- " + f"{item.item_name}".ljust(15) + f"{item.quantity}".ljust(5) + f"{item.description[:20]}".ljust(20) + f"{item.category}".ljust(20) + f"{item.warehouse}".ljust(15))
+        products.textLine(line)
+        i+=1
+
+    #for line in lines:
+     #   products.textLine(line)
+
+    c.drawText(products)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+    filename = f'{warehouse.warehouse_name}.pdf'
+    return FileResponse(buf, as_attachment=True, filename=filename)
