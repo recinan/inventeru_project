ab804dd6bf05d95fc23436fe538146364468ed5c
Recep Inan <recinan.ceng@gmail.com>
2024-12-13 09:41:38 +0300

    warehouse detail pdf view category slug error has solved

--- inventory_management_con/warehouse_man/urls.py
+++ inventory_management_con/warehouse_man/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('add-warehouse',views.add_warehouse,name='add-warehouse'),
     path('edit-warehouse/<slug:warehouse_slug>',views.edit_warehouse,name='edit-warehouse'),
     path('warehouse-detail/<slug:warehouse_slug>',views.warehouse_detail,name='warehouse-detail'),
+    path('warehouse-detail-pdf/<slug:warehouse_slug>',views.warehouse_detail_pdf,name='warehouse-detail-pdf'),
     path('warehouse-detail-pdf/<slug:warehouse_slug>/<slug:category_slug>',views.warehouse_detail_pdf,name='warehouse-detail-pdf')
 
 ]
