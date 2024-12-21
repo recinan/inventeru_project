25aeb59af50494b6062abc9193f6c94d005948e3
Recep Inan <recinan.ceng@gmail.com>
2024-12-21 16:08:43 +0300

    changes

--- inventory_management_con/warehouse_man/urls.py
+++ inventory_management_con/warehouse_man/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('edit-warehouse/<slug:warehouse_slug>',views.edit_warehouse,name='edit-warehouse'),
     path('warehouse-detail/<slug:warehouse_slug>',views.warehouse_detail,name='warehouse-detail'),
     path('warehouse-detail-pdf/<slug:warehouse_slug>',views.warehouse_detail_pdf,name='warehouse-detail-pdf'),
-    path('warehouse-detail-pdf/<slug:warehouse_slug>/<slug:category_slug>',views.warehouse_detail_pdf,name='warehouse-detail-pdf')
-
+    path('warehouse-detail-pdf/<slug:warehouse_slug>/<slug:category_slug>',views.warehouse_detail_pdf,name='warehouse-detail-pdf'),
+    path('warehouse-detail/<slug:warehouse_slug>',views.category_per_warehouse_chart, name='category-per-warehouse-chart'),
+    path('warehouse-detail/<slug:warehouse_slug>',views.products_per_category_chart, name='product-per-warehouse-chart')
 ]
--- inventory_management_con/warehouse_man/views.py
+++ inventory_management_con/warehouse_man/views.py
@@ -16,8 +16,13 @@ from reportlab.pdfbase.ttfonts import TTFont
 import folium
 import googlemaps
 from decouple import config
+from django.db import models
+import matplotlib.pyplot as plt
+import matplotlib
+from io import BytesIO
+import base64
 
-
+matplotlib.use('Agg')
 
 # Create your views here.
 
@@ -91,12 +96,14 @@ def warehouse_location(address):
 @login_required(login_url='login')
 def warehouse_detail(request, warehouse_slug):
     warehouse = get_object_or_404(Warehouse, user = request.user,slug = warehouse_slug)
-    categories = Category.objects.filter(user = request.user, warehouse=warehouse)
+    categories = Category.objects.filter(user = request.user, warehouse=warehouse).order_by('date_created')
     items = InventoryItem.objects.filter(user = request.user, warehouse=warehouse)
     inventory_items_less_than_five= InventoryItem.objects.filter(user=request.user,warehouse=warehouse, quantity__lte = 5 )
     
     address = f"{warehouse.neighborhood} {warehouse.street} {warehouse.district}/{warehouse.city} {warehouse.country} {warehouse.postal_code}"
     m = warehouse_location(address=address)
+    category_chart = category_per_warehouse_chart(request,warehouse_slug)
+    product_chart = products_per_category_chart(request,warehouse_slug)
     if request.method == "POST":
         warehouse = warehouse
         form = WarehouseForm(request.POST, instance = warehouse)
@@ -109,6 +116,11 @@ def warehouse_detail(request, warehouse_slug):
         
     if warehouse:
         form = WarehouseForm(instance=warehouse)
+
+    CHART_OPTIONS = {
+    'options1': 'Categories in warehouse',
+    'options2': 'Products in warehouse'
+    }
     
     context = {
         'map':m,
@@ -116,7 +128,10 @@ def warehouse_detail(request, warehouse_slug):
         'warehouse':warehouse,
         'categories':categories,
         'items':items,
-        'less_items': inventory_items_less_than_five
+        'less_items': inventory_items_less_than_five,
+        'category_chart':category_chart,
+        'product_chart':product_chart,
+        'chart_options':CHART_OPTIONS
     }
     
     return render(request, 'warehouse_man/warehouse-detail.html',context)
@@ -217,3 +232,90 @@ def delete_warehouse(request, warehouse_slug):
         'warehouse':item
     }
     return render(request, 'warehouse_man/delete_warehouse.html',context)
+
+def category_per_warehouse_chart(request, warehouse_slug):
+    warehouse = get_object_or_404(Warehouse, user=request.user, slug=warehouse_slug)
+    categories = Category.objects.filter(warehouse=warehouse)
+    
+    category_data = {}
+    for category in categories:
+        max_quantity = InventoryItem.objects.filter(user=request.user, category=category).aggregate(total=models.Sum('quantity'))['total'] or 0
+        if max_quantity > 0:
+            category_data[category.category_name] = max_quantity
+    labels = list(category_data.keys())
+    sizes = list(category_data.values())
+    if not sizes:
+        sizes = list()
+        plt.figure(figsize=(4,4))
+        plt.pie(sizes, labels=labels,autopct='%1.1f%%',startangle=140,textprops={'fontsize':10}, labeldistance=0.8)
+        plt.title('Categories per warehouse')
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+        image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+        buffer.close()
+        plt.close()
+        return image_base64
+    
+    plt.figure(figsize=(4,4))
+    plt.pie(sizes, labels=labels,autopct='%1.1f%%',startangle=140)
+    plt.title('Categories per warehouse')
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    buffer.close()
+    plt.close()
+    return image_base64
+
+def products_per_category_chart(request, warehouse_slug):
+    warehouse = get_object_or_404(Warehouse, user=request.user, slug=warehouse_slug)
+    category_slug = request.GET.get('selected_category')
+    if not category_slug:
+        category = Category.objects.filter(user=request.user,warehouse=warehouse).first()
+        category_slug = category.slug
+    category = get_object_or_404(Category, user=request.user, warehouse=warehouse, slug=category_slug)
+    inventory_items = InventoryItem.objects.filter(user=request.user, warehouse=warehouse, category=category)
+    total_quantity = inventory_items.aggregate(total=models.Sum('quantity'))['total'] or 0
+    item_data = {}  
+    for item in inventory_items:
+        if item.quantity > 0:
+            item_data[item.item_name] = item.quantity
+    labels = list(item_data.keys())
+    sizes = list(item_data.values())
+    threshold = 5
+    filtered_labels = []
+    percentages = []
+    if total_quantity > 0:
+        for size in sizes:
+            percentage = (size/total_quantity)*100
+            percentages.append(percentage)
+        for label, percentage in zip(labels, percentages):
+            if percentage >= threshold:
+                filtered_labels.append(label)
+            else:
+                filtered_labels.append("")
+    else:
+        filtered_labels=[]
+    if not sizes:
+        sizes = list()
+        plt.figure(figsize=(4,4))
+        plt.pie(sizes, labels=filtered_labels, autopct='%1.1f%%',startangle=140)
+        plt.title(f'Products per {category.category_name}')
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+        image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+        buffer.close()
+        plt.close()
+        return image_base64
+    plt.figure(figsize=(4,4))
+    plt.pie(sizes, labels=filtered_labels, autopct='%1.1f%%', startangle=140,textprops={'fontsize':10}, labeldistance=0.8)
+    plt.title(f'Products per {category.category_name}')
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    buffer.close()
+    plt.close()
+    return image_base64
