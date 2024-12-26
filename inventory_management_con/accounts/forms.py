c2b553eee60ece24ae74d227c4c79809dd217416
Recep Inan <recinan.ceng@gmail.com>
2024-12-27 02:53:44 +0300

    warehouse clean phone number added

--- inventory_management_con/accounts/forms.py
+++ inventory_management_con/accounts/forms.py
@@ -4,15 +4,31 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model
 from django_recaptcha.fields import ReCaptchaField
 from django_recaptcha.widgets import ReCaptchaV2Checkbox
+from django.core.exceptions import ValidationError
+import re
 
 class UserRegisterForm(UserCreationForm):
 
     email = forms.EmailField(help_text="A valid email address for verification, please.", required=True)
     username = forms.CharField(help_text="Choose a unique username, please.", required=True)
-    phone_number = forms.CharField(help_text="Choose a unique phone number, please.", required=True)
+    phone_number = forms.CharField(help_text="Choose a unique phone number, please.", required=True, widget=forms.TextInput(attrs={'placeholder':'(XXX) XXX XXXX'}))
     class Meta:
         model = get_user_model()
         fields = ['user_image','first_name','last_name','username','email','phone_number','password1','password2']
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+
+        if phone_number:
+            clean_number = re.sub(r'\D','',phone_number)
+
+            if len(clean_number) != 10:
+                raise ValidationError('Phone number must be 10 or 11 digits long!')
+            
+            formatted_number = f"({clean_number[:3]}) {clean_number[3:6]} {clean_number[6:10]}"
+            return formatted_number
+        
+        return phone_number
 
     def save(self, commit=True):
         user = super(UserRegisterForm, self).save(commit=False)
@@ -44,4 +60,18 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ['user_image','first_name','last_name','email','phone_number']
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+
+        if phone_number:
+            clean_number = re.sub(r'\D','',phone_number)
+
+            if len(clean_number) != 10:
+                raise ValidationError('Phone number must be 10 or 11 digits long!')
+            
+            formatted_number = f"({clean_number[:3]}) {clean_number[3:6]} {clean_number[6:10]}"
+            return formatted_number
+        
+        return phone_number
 
--- inventory_management_con/warehouse_man/forms.py
+++ inventory_management_con/warehouse_man/forms.py
@@ -3,10 +3,12 @@ from .models import Warehouse
 from django.core.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
 from django.utils.text import slugify
+import re
 
 
 class WarehouseForm(forms.ModelForm):
     warehouse_name = forms.CharField()
+    phone_number = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'(XXX) XXX XXXX'}))
     class Meta:
         model = Warehouse
         fields = ['warehouse_name','phone_number','neighborhood','street','district','city','postal_code','country']
@@ -18,7 +20,20 @@ class WarehouseForm(forms.ModelForm):
         if Warehouse.objects.filter(user=self.user,slug=slug).exclude(pk=self.instance.pk).exists():
             raise ValidationError(f'{warehouse_name} already exists!')
         return warehouse_name
-   
+    
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+
+        if phone_number:
+            clean_number = re.sub(r'\D','',phone_number)
+
+            if len(clean_number) != 10:
+                raise ValidationError('Phone number must be 10 or 11 digits long!')
+            
+            formatted_number = f"({clean_number[:3]}) {clean_number[3:6]} {clean_number[6:10]}"
+            return formatted_number
+        
+        return phone_number
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user',None)
@@ -28,8 +43,11 @@ class WarehouseForm(forms.ModelForm):
         instance = super().save(commit=False)
         if self.user:
             instance.user = self.user
+       
         if commit:
             instance.save()
         return instance
+    
+
     
 
